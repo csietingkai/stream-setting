3f432a7bff0f5b66c8a5519d2a2991dc97d1cb8a
tingkai <tingkai@csie.io>
2022-03-30 15:48:25 +0800

    test subage

--- subage.js
+++ subage.js
@@ -48,8 +48,8 @@ const subJson = JSON.stringify(subsInfo);
 
 const getMessage = (language = 'tw') => {
 	const pattern = {
-		en: `$(eval is=$(urlfetch json https://bit.ly/3Dnpujc);i=is.find(x=>x.name=='$(user)');n=i?.nick||'$(user)';d=((Date.now()-i?.subDate)/(86400000));i?"$(n) has sub $(d) days!":"$(user) no sub (☍﹏⁰)")`,
-		tw: `$(eval is=$(urlfetch json https://bit.ly/3Dnpujc);i=is.find(x=>x.name=='$(user)');n=i?.nick||'$(user)';d=((Date.now()-i?.subDate)/(86400000));i?"$(n)已訂閱$(d)天！":"$(user)沒訂閱(☍﹏⁰)")`
+		en: `$(eval is=$(urlfetch json https://bit.ly/3Dnpujc);i=is.find(x=>x.name=='$(user)');n=i?.nick||'$(user)';d=((Date.now()-i?.subDate)/(86400000));i?n+" has sub "+d.toFixed(0)+" days!":"$(user) no sub (☍﹏⁰)")`,
+		tw: `$(eval is=$(urlfetch json https://bit.ly/3Dnpujc);i=is.find(x=>x.name=='$(user)');n=i?.nick||'$(user)';d=((Date.now()-i?.subDate)/(86400000));i?n+"已訂閱"+d.toFixed(0)+"天！":"$(user)沒訂閱(☍﹏⁰)")`
 	}
 	return pattern[language];
 }
