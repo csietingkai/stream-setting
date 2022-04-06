b7c32622daf684966c9508bf753a61ee79592244
tingkai <tingkai@csie.io>
2022-04-06 15:27:59 +0800

    shorter sub command

--- get-commands.js
+++ get-commands.js
@@ -16,8 +16,8 @@ const ddhighlight = (language = 'tw') => {
 
 const subage = (language = 'tw') => {
 	const pattern = {
-		en: `$(eval is=$(urlfetch json https://bit.ly/3Dnpujc);i=is.find(x=>x.name=='$(user)');n=i?.nick||'$(user)';d=((Date.now()-i?.subDate)/(86400000));i?n+" has sub "+d.toFixed(0)+" days!":"$(user) no sub (☍﹏⁰)")`,
-		tw: `$(eval is=$(urlfetch json https://bit.ly/3Dnpujc);i=is.find(x=>x.name=='$(user)');n=i?.nick||'$(user)';d=((Date.now()-i?.subDate)/(86400000));i?n+"已訂閱"+d.toFixed(0)+"天！":"$(user)沒訂閱(☍﹏⁰)")`
+		en: `$(eval is=$(urlfetch json https://bit.ly/3Dnpujc);i=is.find(x=>x.name=='$(user)');d=((Date.now()-i?.subDate)/(86400000));i?i.nick+" has sub "+d.toFixed(0)+" days!":"$(user) no sub (☍﹏⁰)")`,
+		tw: `$(eval is=$(urlfetch json https://bit.ly/3Dnpujc);i=is.find(x=>x.name=='$(user)');d=((Date.now()-i?.subDate)/(86400000));i?i.nick+"已訂閱"+d.toFixed(0)+"天！":"$(user)沒訂閱(☍﹏⁰)")`
 	}
 	return pattern[language];
 }
