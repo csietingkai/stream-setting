96ba9728af50b0cfa4654ea907bf51cd934c5195
tingkai <tingkai@csie.io>
2023-08-08 14:06:15 +0800

    superchat

--- superchat/sc.js
+++ superchat/sc.js
@@ -1,9 +1,33 @@
 const init = () => {
     const queryString = window.location.search;
-    console.log(queryString);
-    const urlParams = new URLSearchParams(queryString);
-    const product = urlParams.get('product')
-    console.log(product);
+    const params = new URLSearchParams(queryString);
+    const price = params.get('price');
+
+    $('#author-name').text(params.get('name'));
+    $('#purchase-amount').text(numberComma(price));
+    $('#message').text(params.get('message'));
+
+    const scColor = color(price);
+    if (scColor) {
+        $('#card').addClass(color(price));
+    }
+}
+
+const color = (price) => {
+    if (!price || isNaN(price)) {
+        return '';
+    }
+    if (price < 75) {
+        return 'blue';
+    } else if (price >= 75 && price < 150) {
+        return 'green';
+    } else if (price >= 150 && price < 300) {
+        return 'yellow';
+    } else if (price >= 300 && price < 1500) {
+        return 'orange';
+    } else if (price >= 1500) {
+        return 'red';
+    }
 }
 
 const numberComma = (num) => {
